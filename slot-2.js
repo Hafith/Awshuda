539e7cec94254b0ba7747c2de90e6eb38362dab4
Hafith <88893624+Hafith@users.noreply.github.com>
2021-11-22 00:15:57 +0700

    Update slot-2.js

--- slot-2.js
+++ slot-2.js
@@ -1,1 +1,16 @@
 
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1662098130761985"
+     crossorigin="anonymous"></script>
+
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1662098130761985"
+     crossorigin="anonymous"></script>
+<!-- thebootstrappersguide responsif -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1662098130761985"
+     data-ad-slot="8304270628"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
